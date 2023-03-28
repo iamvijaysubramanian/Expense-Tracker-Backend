89176d48ec2dcfb251444e7632110d9ed91ed2b7
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-03-28 14:57:01 +0530

    commit

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-require("dotenv").config();
-const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
 const connectDb = require("./config/connectDb");
-
-
-
+// config dot env file
+dotenv.config();
 
 //databse call
 connectDb();
@@ -17,17 +16,13 @@ const app = express();
 //middlewares
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({
-    origin:"*",
-})
-);
+app.use(cors());
 
 //routes
+//user routes
 app.use("/api/v1/users", require("./routes/userRoute"));
-
 //transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
-
 //static files
 // app.use(express.static(path.resolve(__dirname, "../build")));
 
@@ -36,7 +31,7 @@ app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 // });
 
 //port
-const PORT =process.env.PORT || 8080;
+const PORT = 8080 || process.env.PORT;
 
 //listen server
 app.listen(PORT, () => {
